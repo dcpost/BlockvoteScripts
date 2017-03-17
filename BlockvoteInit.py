60e56ba71757c2dcf291d95b0ea7e905e8f22a18
dcpost <dcpost@ucalgary.ca>
2017-03-16 18:47:11 -0600

    First commit

---
+++ BlockvoteInit.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+import requests
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("username", help="The username")
+parser.add_argument("password", help="The password")
+args = parser.parse_args()
+
+payload = {"client_id" : "f2pQL6jMgGQLDsNlHfhQgsmMVGzMcgmg",
+	"username" : args.username,
+	"password" : args.password,
+	"id_token" : "null",
+	"connection" : "Username-Password-Authentication",
+	"grant_type" : "password",
+	"scope" : "openid",
+	"device" : "null"}
+	
+loginRequest = requests.get('https://enel500blockvote.auth0.com/oauth/ro', data = payload)
+
+r=loginRequest.json()
+id_token="Bearer "+r['id_token']
+access_token=r['access_token']
+
+header = {"Authorization" : id_token,
+	"AccessToken" : access_token}
+	
+initRequest = requests.get('https://blockvotenode2.mybluemix.net/init', headers = header)
+print(initRequest)
+r=initRequest.json()
+print(r)
---
+++ BlockvoteRegistrar.py
@@ -0,0 +1,105 @@
+import argparse
+import sys
+import requests
+import json
+import base64
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from subprocess import *
+
+def jarWrapper(*args):
+    process = Popen(['java', '-jar']+list(args), stdout=PIPE, stderr=PIPE)
+    ret = []
+    while process.poll() is None:
+        line = process.stdout.readline()
+        if line != '' and line.endswith('\n'):
+            ret.append(line[:-1])
+    stdout, stderr = process.communicate()
+    ret += stdout.split('\n')
+    if stderr != '':
+        ret += stderr.split('\n')
+    ret.remove('')
+    return ret
+	
+parser = argparse.ArgumentParser()
+parser.add_argument("username", help="The admin username")
+parser.add_argument("password", help="The admin password")
+parser.add_argument("rname", help="The registrar name")
+parser.add_argument("remail", help="The registrar email")
+parser.add_argument("rpassword", help="The new password")
+parser.add_argument("region", help="the region")
+parser.add_argument("district", help="the registrar district")
+
+args = parser.parse_args()
+
+#Get API token
+payload = {"grant_type":"client_credentials",
+"client_id": "mjgRuEI10KlWWa7kAic7m6sD75FV0X81",
+"client_secret": "gjNBs1NbFzX1xWw-zhBexGJpqZD-_hF0lK5Pg5H3yCSscx7bg-kZiLOJT3Ai5blt",
+"audience": "https://enel500blockvote.auth0.com/api/v2/"
+}
+
+apiTokenRequest = requests.post("https://enel500blockvote.auth0.com/oauth/token", data = payload)
+print(apiTokenRequest)
+r=apiTokenRequest.json()
+print(r)
+api_token="Bearer "+r['access_token']
+
+#Get the login info
+payload = {"client_id" : "f2pQL6jMgGQLDsNlHfhQgsmMVGzMcgmg",
+	"username" : args.username,
+	"password" : args.password,
+	"id_token" : "null",
+	"connection" : "Username-Password-Authentication",
+	"grant_type" : "password",
+	"scope" : "openid",
+	"device" : "null"}
+	
+loginRequest = requests.post('https://enel500blockvote.auth0.com/oauth/ro', data = payload)
+
+r=loginRequest.json()
+print(r)
+id_token="Bearer "+r['id_token']
+access_token=r['access_token']
+
+#generate key
+javaargs = ['RSAGenerator.jar'] 
+result = jarWrapper(*javaargs)
+publicExponent = result[0]
+privateExponent = result[1]
+modulus = result[2]
+
+#create the user on auth0
+Auth0Header = {"Authorization":api_token}
+
+payload = {"connection": "Username-Password-Authentication",
+  "email": args.remail,
+  "password": args.rpassword,
+  "user_metadata": {
+	  "publicKeyExponent": publicExponent,
+	  "publicKeyModulus": modulus,
+	  "privateKeyModulus": modulus,
+	  "privateKeyExponent": privateExponent
+	},
+  "app_metadata": {}
+  }
+  
+createRequest = requests.post('https://enel500blockvote.auth0.com/api/v2/users', headers = Auth0Header,  json = payload)
+print(createRequest)
+r=createRequest.json()
+print(r)
+#Create the user on Blockvote 
+BlockvoteHeader = {"Authorization" : id_token,
+	"AccessToken" : access_token}
+	
+payload = {"region":args.region,
+	"registrarName":args.rname,
+	"registrarKeyModulus": modulus,
+	"registrarKeyExponent":publicExponent,
+	"registrarDistrict":args.district}
+	
+initRequest = requests.post('https://blockvotenode2.mybluemix.net/addRegistrar', headers = BlockvoteHeader, data = payload)
+print(initRequest)
+r=initRequest.json()
+print(r)
